751632cfc63b6bb67dbfae68e9a5883aad220f9e
Devon Bahary <devon.bahary@robinhealthcare.com>
2021-12-26 00:45:27 -0600

    lint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,23 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
+        node: true,
     },
-    extends: ['airbnb-base'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'prettier',
+    ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 13,
         sourceType: 'module',
-    },
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+    },   
     plugins: ['@typescript-eslint'],
-    rules: {},
+    rules: {
+        '@typescript-eslint/explicit-function-return-type': 'error',
+    },
 };
