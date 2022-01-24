4025ca586a78be6bdb1fef4e63e61726b7ee6f04
Devon Bahary <devon.bahary@robinhealthcare.com>
2022-01-23 18:31:14 -0800

    adds rails environment

---
+++ src/game/environments/rails.ts
@@ -0,0 +1,36 @@
+import { CircleBody } from 'src/physics/bodies/CircleBody';
+import { RectBody } from 'src/physics/bodies/RectBody';
+import { Vector } from 'src/physics/Vector';
+import { World } from 'src/physics/World';
+import { GameEntity } from '../GameEntity';
+import { scaleToPhysicsLength } from '../utilities';
+
+const settings = {
+    world: {
+        tileWidth: 5,
+        tileHeight: 5,
+        noFriction: true,
+    },
+};
+
+export const setupRails = (): [World, GameEntity] => {
+    const world = new World({
+        width: scaleToPhysicsLength(settings.world.tileWidth),
+        height: scaleToPhysicsLength(settings.world.tileHeight),
+        noFriction: settings.world.noFriction,
+    });
+
+    const player = new GameEntity(world, new CircleBody({ elasticity: 1 }));
+    player.speed = 2;
+    player.body.name = 'player';
+
+    player.moveTo(new Vector(1, Math.floor(settings.world.tileHeight / 2)));
+
+    world.addBody(player.body);
+
+    const gameEntity = new GameEntity(world, new RectBody({ elasticity: 1 }));
+    world.addBody(gameEntity.body);
+    gameEntity.moveTo(new Vector(3, 2));
+
+    return [world, player];
+};
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { Controls } from './game/Controls';
 import { Renderer } from './game/Renderer';
 import { setupChaos } from './game/environments/chaos';
+import { setupRails } from './game/environments/rails';
 
-const [world, player] = setupChaos();
+const [world, player] = setupRails();
 
 const renderer = new Renderer(world, player.body);
 const controls = new Controls(player);
